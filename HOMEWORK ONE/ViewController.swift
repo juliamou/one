58073235f54d2ffaeaff42944c9a5f89aa913561
Julia J Mou <jmou@acs-wlh2015-20.ucsd.edu>
2016-04-13 15:33:40 -0700

    Initial Commit

---
+++ HOMEWORK ONE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HOMEWORK ONE
+//
+//  Created by Julia J Mou on 4/13/16.
+//  Copyright (c) 2016 Julia J Mou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
